9f7b689fd9c3c9537936c233e995f95e174cae82
Sambhav Jain <sambhavjain2612@gmail.com>
2018-01-23 20:05:40 +0530

    working read

--- read.php
+++ read.php
@@ -1,3 +1,5 @@
+
+<?php require "templates/header.php"; ?>
 <?php
 
 	if (isset($_POST['submit'])) 
@@ -8,18 +10,18 @@
 			require "common.php";
 
 			$connection = new PDO($dsn, $username, $password, $options);
-
-			$sql = "SELECT * 
-					FROM users
-					WHERE location = :location";
-
+			
 			$location = $_POST['location'];
+			$sql = 'SELECT * 
+					FROM users
+					WHERE `location`="'.$location.'"';
 
 			$statement = $connection->prepare($sql);
 			$statement->bindParam(':location', $location, PDO::PARAM_STR);
 			$statement->execute();
 
 			$result = $statement->fetchAll();
+			printData($result);
 		}
 		
 		catch(PDOException $error) 
@@ -29,13 +31,9 @@
 	}
 ?>
 
-<?php require "templates/header.php"; ?>
-
 <?php  
-	if (isset($_POST['submit'])) 
-	{
-		if ($result && $statement->rowCount() > 0) 
-		{ ?>
+		function printData($result) {
+	 ?>
 			<h2>Results</h2>
 
 			<table>
@@ -53,28 +51,22 @@
 				<tbody>
 		<?php 
 			foreach ($result as $row) 
-			{ ?>
+			{?>
 				<tr>
-					<td><?php echo escape($row["id"]); ?></td>
-					<td><?php echo escape($row["firstname"]); ?></td>
-					<td><?php echo escape($row["lastname"]); ?></td>
-					<td><?php echo escape($row["email"]); ?></td>
-					<td><?php echo escape($row["age"]); ?></td>
-					<td><?php echo escape($row["location"]); ?></td>
-					<td><?php echo escape($row["date"]); ?> </td>
+					<td><?php echo $row["id"]; ?></td>
+					<td><?php echo $row["firstname"]; ?></td>
+					<td><?php echo $row["lastname"]; ?></td>
+					<td><?php echo $row["email"]; ?></td>
+					<td><?php echo $row["age"]; ?></td>
+					<td><?php echo $row["location"]; ?></td>
+					<td><?php echo $row["date"]; ?> </td>
 				</tr>
 			<?php 
 			} ?>
 			</tbody>
 		</table>
 		<?php 
-		} 
-		else 
-		{ ?>
-			<blockquote>No results found for <?php echo escape($_POST['location']); ?>.</blockquote>
-		<?php
-		} 
-	}
+		}
 ?> 
 
 <h2>Find user based on location</h2>
